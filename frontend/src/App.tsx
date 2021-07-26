6864c0dcda34bdf7fe1dec5f3bff1658a2e856f7
duc <1025434218@qq.com>
2021-07-26 09:48:19 +0800

    wip

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useEffect, useState } from "react";
+import React, { FC, useEffect, useState, useCallback } from "react";
 import { Layout } from "antd";
 import AppContent from "./components/AppContent";
 import { WsContext } from "./contexts/useWebsocket";
@@ -6,7 +6,7 @@ import { handleCreateOrUpdateProjects } from "./store/actions";
 import { isJsonString } from "./utils/json";
 import { useDispatch } from "react-redux";
 import { Switch, Route, Link } from "react-router-dom";
-import GitlabProjectManager from './components/GitlabProjectManager'
+import GitlabProjectManager from "./components/GitlabProjectManager";
 
 const { Header, Content, Footer } = Layout;
 
@@ -22,46 +22,55 @@ const App: FC = () => {
   const dispatch = useDispatch();
   const [ws, setWs] = useState<any>();
 
+  const connectWs = useCallback(() => {
+    console.log("ws init");
+    let url: string = process.env.REACT_APP_WS_URL
+      ? process.env.REACT_APP_WS_URL
+      : "";
+    if (url === "") {
+      let isHttps = "https:" === window.location.protocol ? true : false;
+      url = `${isHttps ? "wss" : "ws"}://${window.location.host}/ws`;
+    }
+    let conn = new WebSocket(url);
+    setWs(conn);
+    conn.onopen = function (evt) {
+      console.log("ws onopen");
+    };
+    conn.onclose = function (evt) {
+      setWs(null);
+      console.log("ws closed");
+    };
+    conn.onmessage = function (evt) {
+      if (!isJsonString(evt.data)) {
+        return;
+      }
+      let data: WsResponse = JSON.parse(evt.data);
+      console.log(data, data.type);
+      dispatch(handleCreateOrUpdateProjects(data.slug, data));
+      console.log("onmessage", evt.data);
+    };
+  }, [dispatch]);
+
   useEffect(() => {
     if (!ws) {
-      console.log("ws init");
-      let url: string = process.env.REACT_APP_WS_URL
-        ? process.env.REACT_APP_WS_URL
-        : "";
-      if (url === "") {
-        let isHttps = 'https:' === window.location.protocol ? true: false;
-        url = `${isHttps ? "wss" : "ws"}://${window.location.host}/ws`;
-      }
-      let conn = new WebSocket(url);
-      setWs(conn);
-      conn.onopen = function (evt) {
-        console.log("ws onopen");
-      };
-      conn.onclose = function (evt) {
-        setWs(null);
-        console.log("ws closed");
-      };
-      conn.onmessage = function (evt) {
-        if (!isJsonString(evt.data)) {
-          return;
-        }
-        let data: WsResponse = JSON.parse(evt.data);
-        console.log(data, data.type);
-        dispatch(handleCreateOrUpdateProjects(data.slug, data));
-        console.log("onmessage", evt.data);
-      };
+      connectWs();
     }
-  }, [ws, dispatch]);
+  }, [connectWs, ws]);
 
   return (
     <WsContext.Provider value={ws}>
       <Layout className="app">
         <Header style={{ position: "fixed", zIndex: 1, width: "100%" }}>
-          <Link to="/" className="app-title">Mars</Link>
+          <Link to="/" className="app-title">
+            Mars
+          </Link>
         </Header>
         <Content className="app-content">
           <Switch>
-            <Route path="/web/gitlab_project_manager" component={GitlabProjectManager} />
+            <Route
+              path="/web/gitlab_project_manager"
+              component={GitlabProjectManager}
+            />
             <Route path="*">
               <AppContent />
             </Route>
