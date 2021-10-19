ca9584ac97798eaa42998397333a8e6e4ba9f70a
duc <1025434218@qq.com>
2021-10-19 19:43:14 +0800

    wip

--- main.go
+++ main.go
@@ -4,9 +4,6 @@ import (
 	_ "embed"
 
 	"github.com/duc-cnzj/mars/cmd"
-	_ "github.com/duc-cnzj/mars/internal/plugins/docker"
-	_ "github.com/duc-cnzj/mars/internal/plugins/domain_resolver"
-	_ "github.com/duc-cnzj/mars/internal/plugins/wssender"
 )
 
 //go:embed config_example.yaml
---
+++ plugins.go
@@ -0,0 +1,7 @@
+package main
+
+import (
+	_ "github.com/duc-cnzj/mars/internal/plugins/docker"
+	_ "github.com/duc-cnzj/mars/internal/plugins/domain_resolver"
+	_ "github.com/duc-cnzj/mars/internal/plugins/wssender"
+)
