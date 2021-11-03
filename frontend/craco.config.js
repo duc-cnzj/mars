7c11ed1e5b6d0aa8ed0b9ed7a2d7d529f00c7960
duc <1025434218@qq.com>
2021-11-03 15:45:17 +0800

    wip

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -9,7 +9,7 @@ module.exports = {
     }) => {
       webpackConfig.output = {
         ...webpackConfig.output,
-        publicPath: process.env.NODE_ENV === 'production' ? '/resources' : '/',
+        publicPath: process.env.NODE_ENV === 'production' ? '/resources/' : '/',
       }
       return webpackConfig
     }
