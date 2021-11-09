3e7455bff4e76e4893fec2f898061c37c6328542
duc <1025434218@qq.com>
2021-11-09 12:25:13 +0800

    fix

--- internal/utils/panic.go
+++ internal/utils/panic.go
@@ -2,7 +2,7 @@ package utils
 
 import "github.com/duc-cnzj/mars/internal/mlog"
 
-func HandlePanic()  {
+func HandlePanic() {
 	err := recover()
 	if err != nil {
 		mlog.Errorf("[Panic]: %v", err)
