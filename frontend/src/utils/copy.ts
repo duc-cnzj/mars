e7b4a87c566337428702d54d73f5c9a09e900a48
duc <1025434218@qq.com>
2022-03-31 16:18:03 +0800

    剪切板兼容

--- frontend/src/utils/copy.ts
+++ frontend/src/utils/copy.ts
@@ -3,8 +3,20 @@ import { message } from "antd";
 export function copy(text: string, msg?: string) {
   if (navigator.clipboard) {
     navigator.clipboard.writeText(text);
-    message.success(msg ? msg : "已复制");
   } else {
-    message.error("你的浏览器不支持该操作");
+    // 创建text area
+    let textArea = document.createElement("textarea");
+    textArea.value = text;
+    // 使text area不在viewport，同时设置不可见
+    textArea.style.position = "absolute";
+    textArea.style.opacity = "0";
+    textArea.style.left = "-999999px";
+    textArea.style.top = "-999999px";
+    document.body.appendChild(textArea);
+    textArea.focus();
+    textArea.select();
+    document.execCommand("copy");
+    textArea.remove();
   }
+  message.success(msg ? msg : "已复制");
 }
