0d9e6280c632a5301af93d437a08b345558f06cb
duc <1025434218@qq.com>
2022-04-01 18:14:47 +0800

    wip

--- internal/database/manager.go
+++ internal/database/manager.go
@@ -2,6 +2,8 @@ package database
 
 import (
 	"github.com/duc-cnzj/mars/internal/contracts"
+	"github.com/duc-cnzj/mars/internal/mlog"
+	"github.com/duc-cnzj/mars/internal/models"
 	"gorm.io/gorm"
 )
 
@@ -23,6 +25,50 @@ func (m *Manager) SetDB(db *gorm.DB) {
 }
 
 func (m *Manager) AutoMigrate(dst ...any) error {
+	var migrateV3ToV4 = "migrating: v3 => v4 start."
+
+	// v3 => v4 start.
+	if m.db.Migrator().HasColumn(&models.Changelog{}, "gitlab_project_id") {
+		// changelogs rename gitlab_project_id => git_project_id
+		if err := m.db.Migrator().RenameColumn(&models.Changelog{}, "gitlab_project_id", "git_project_id"); err != nil {
+			mlog.Warningf("[%s]: err: %v", migrateV3ToV4, err)
+		}
+	}
+
+	if m.db.Migrator().HasTable("gitlab_projects") {
+		// GitlabProject rename GitProject
+		if err := m.db.Migrator().RenameTable("gitlab_projects", &models.GitProject{}); err != nil {
+			mlog.Warningf("[%s]: err: %v", migrateV3ToV4, err)
+		}
+		if m.db.Migrator().HasColumn(&models.GitProject{}, "gitlab_project_id") {
+			// gitlab_project_id => git_project_id
+			if err := m.db.Migrator().RenameColumn(&models.GitProject{}, "gitlab_project_id", "git_project_id"); err != nil {
+				mlog.Warningf("[%s]: err: %v", migrateV3ToV4, err)
+			}
+		}
+	}
+
+	// projects
+	// gitlab_project_id git_project_id
+	if m.db.Migrator().HasColumn(&models.Project{}, "gitlab_project_id") {
+		if err := m.db.Migrator().RenameColumn(&models.Project{}, "gitlab_project_id", "git_project_id"); err != nil {
+			mlog.Warningf("[%s]: err: %v", migrateV3ToV4, err)
+		}
+	}
+	// gitlab_branch git_branch
+	if m.db.Migrator().HasColumn(&models.Project{}, "gitlab_branch") {
+		if err := m.db.Migrator().RenameColumn(&models.Project{}, "gitlab_branch", "git_branch"); err != nil {
+			mlog.Warningf("[%s]: err: %v", migrateV3ToV4, err)
+		}
+	}
+	// gitlab_commit git_commit
+	if m.db.Migrator().HasColumn(&models.Project{}, "gitlab_commit") {
+		if err := m.db.Migrator().RenameColumn(&models.Project{}, "gitlab_commit", "git_commit"); err != nil {
+			mlog.Warningf("[%s]: err: %v", migrateV3ToV4, err)
+		}
+	}
+	// v3 => v4 end.
+
 	if err := m.db.AutoMigrate(dst...); err != nil {
 		return err
 	}
