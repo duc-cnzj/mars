7db5f6a234f20f7b1745a37555978a479b91e217
duc <1025434218@qq.com>
2022-04-07 22:41:20 +0800

    bugfix

--- internal/cache/db_cache.go
+++ internal/cache/db_cache.go
@@ -1,12 +1,13 @@
 package cache
 
 import (
+	"encoding/base64"
+	"fmt"
 	"time"
 
 	"github.com/duc-cnzj/mars/internal/contracts"
 	"github.com/duc-cnzj/mars/internal/mlog"
 	"github.com/duc-cnzj/mars/internal/models"
-	"gorm.io/gorm"
 )
 
 type DBCache struct {
@@ -18,31 +19,32 @@ func NewDBCache(app contracts.ApplicationInterface) *DBCache {
 }
 
 func (c *DBCache) Remember(key string, seconds int, fn func() ([]byte, error)) ([]byte, error) {
-	var cache models.DBCache
-	if err := c.app.DBManager().DB().Transaction(func(db *gorm.DB) error {
-		db.Raw(
-			"SELECT `id`, `key`, `value`, `expired_at` from `db_cache` where `key` = ? and `expired_at` >= ? and `deleted_at` IS NULL for update",
-			key,
-			time.Now()).Scan(&cache)
-		mlog.Debugf("CacheRemember: %s, from cache: %t", key, cache.ID > 0)
+	do, err, _ := c.app.Singleflight().Do(fmt.Sprintf("cache-remember-%s", key), func() (any, error) {
+		var cache models.DBCache
+		c.app.DBManager().DB().Where("`key` = ? and `expired_at` >= ?", key, time.Now()).First(&cache)
 		if cache.ID > 0 {
-			return nil
+			bs, err := base64.StdEncoding.DecodeString(cache.Value)
+			if err == nil {
+				return bs, nil
+			}
 		}
 		bytes, err := fn()
 		if err != nil {
-			return err
+			return nil, err
 		}
+		toString := base64.StdEncoding.EncodeToString(bytes)
 		cache = models.DBCache{
 			Key:       key,
-			Value:     string(bytes),
+			Value:     toString,
 			ExpiredAt: time.Now().Add(time.Duration(seconds) * time.Second),
 		}
-		if err = db.Create(&cache).Error; err != nil {
+		if err = c.app.DBManager().DB().Create(&cache).Error; err != nil {
 			mlog.Error(err)
 		}
-		return nil
-	}); err != nil {
+		return bytes, nil
+	})
+	if err != nil {
 		return nil, err
 	}
-	return []byte(cache.Value), nil
+	return do.([]byte), nil
 }
