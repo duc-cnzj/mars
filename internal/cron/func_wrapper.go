6d86155121b99cfa154d42be208c267ee1af8b84
duc <1025434218@qq.com>
2022-08-23 12:26:28 +0800

    sleep 0-200 Millisecond

--- internal/cron/func_wrapper.go
+++ internal/cron/func_wrapper.go
@@ -25,7 +25,7 @@ func Wrap(name string, fn func() error, lockerFn func() contracts.Locker) func()
 			metrics.CronPanicCount.With(label).Inc()
 		})
 
-		time.Sleep(time.Duration(rand.Intn(20)) * time.Microsecond)
+		time.Sleep(time.Duration(rand.Intn(200)) * time.Millisecond)
 		releaseFn, acquired := lockerFn().RenewalAcquire(lockKey(name), defaultLockSeconds, defaultRenewSeconds)
 		if acquired {
 			now := time.Now()
