c5a34c27f3f277988f538a7eed38ade6b8ae572b
duc <1025434218@qq.com>
2023-08-14 23:33:39 +0800

    wip

--- frontend/src/components/PodStateTag.tsx
+++ frontend/src/components/PodStateTag.tsx
@@ -5,6 +5,18 @@ import { SyncOutlined, LoadingOutlined } from "@ant-design/icons";
 import { css } from "@emotion/css";
 import theme from "../styles/theme";
 const PodStateTag: React.FC<{ pod: pb.types.StateContainer }> = ({ pod }) => {
+  if (pod.is_old) {
+    return (
+      <Tag
+        icon={<SyncOutlined spin />}
+        color="#fde047"
+        style={{ marginLeft: 5 }}
+      >
+        {pod.pod} 即将停止
+      </Tag>
+    );
+  }
+
   if (pod.terminating) {
     return (
       <Tag
@@ -21,22 +33,10 @@ const PodStateTag: React.FC<{ pod: pb.types.StateContainer }> = ({ pod }) => {
     return (
       <Tag
         icon={<LoadingOutlined spin />}
-        color="#93c5fd"
+        color="#67e8f9"
         style={{ marginLeft: 5 }}
       >
         {pod.pod} 未就绪
-      </Tag>
-    );
-  }
-
-  if (pod.is_old) {
-    return (
-      <Tag
-        icon={<SyncOutlined spin />}
-        color="#fde047"
-        style={{ marginLeft: 5 }}
-      >
-        {pod.pod} 即将停止
       </Tag>
     );
   }
@@ -45,7 +45,7 @@ const PodStateTag: React.FC<{ pod: pb.types.StateContainer }> = ({ pod }) => {
     return (
       <Tag
         icon={<SyncOutlined spin />}
-        color="#818cf8"
+        color="#93c5fd"
         style={{ marginLeft: 5 }}
       >
         {pod.pod} 启动中
