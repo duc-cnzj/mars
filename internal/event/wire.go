7cb654c3bf800c249c2158062b7f1e84ee999807
duc <1025434218@qq.com>
2024-09-02 10:14:41 +0800

    wip

--- internal/event/wire.go
+++ internal/event/wire.go
@@ -1,5 +1,6 @@
 package event
 
+//go:generate mockgen -destination ./mock_event.go -package event github.com/duc-cnzj/mars/v5/internal/event Dispatcher
 import "github.com/google/wire"
 
 var WireEvent = wire.NewSet(NewDispatcher)
