a70cf5e7446194438ddead9048c674a533661cb3
duc <1025434218@qq.com>
2024-09-10 10:07:25 +0800

    wip

--- internal/util/k8s/cp_test.go
+++ internal/util/k8s/cp_test.go
@@ -1,9 +1,13 @@
 package k8s
 
 import (
+	"bytes"
 	"testing"
 
+	"github.com/duc-cnzj/mars/v5/internal/mlog"
 	"github.com/stretchr/testify/assert"
+	"k8s.io/client-go/kubernetes/fake"
+	restclient "k8s.io/client-go/rest"
 )
 
 func TestRemotePathString(t *testing.T) {
@@ -14,4 +18,15 @@ func TestRemotePathString(t *testing.T) {
 func TestStripTrailingSlash(t *testing.T) {
 	assert.Equal(t, "/test/path", stripTrailingSlash("/test/path/"))
 	assert.Equal(t, "/test/path", stripTrailingSlash("/test/path"))
+	assert.Equal(t, "", stripTrailingSlash(""))
+}
+
+func TestNewCopyOptions(t *testing.T) {
+	options := NewCopyOptions(mlog.NewForConfig(nil), &restclient.Config{}, fake.NewSimpleClientset(), 10, &bytes.Buffer{})
+	assert.NotNil(t, options)
+	assert.NotNil(t, options.logger)
+	assert.NotNil(t, options.ClientConfig)
+	assert.NotNil(t, options.Clientset)
+	assert.NotEmpty(t, options.MaxTries)
+	assert.NotNil(t, options.errOut)
 }
